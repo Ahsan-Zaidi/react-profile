75dcdcfec0ca6a0bbf584c59a8eec9f8310f9ec1
Zaidi <zaidiahsan.r@gmail.com>
2023-01-23 15:25:57 -0600

    nav icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -41,9 +44,9 @@ export const NavBar = () => {
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='#'><img src={''} alt='' /></a>
-                            <a href='#'><img src={''} alt='' /></a>
-                            <a href='#'><img src={''} alt='' /></a>
+                            <a href='#'><img src={navIcon1} alt='' /></a>
+                            <a href='#'><img src={navIcon2} alt='' /></a>
+                            <a href='#'><img src={navIcon3} alt='' /></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('Connect')}>
                             <span>Let's Connect</span>
