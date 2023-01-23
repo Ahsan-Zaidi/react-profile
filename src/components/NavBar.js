a0ef0ced839ab43c1bbeb69b308c13494543f0c5
Zaidi <zaidiahsan.r@gmail.com>
2023-01-23 15:52:43 -0600

    logo change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ export const NavBar = () => {
     return (
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="/">
                     <img src={logo} alt='Logo' />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -44,7 +44,7 @@ export const NavBar = () => {
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='#'><img src={navIcon1} alt='' /></a>
+                            <a href=''><img src={navIcon1} alt='' /></a>
                             <a href='#'><img src={navIcon2} alt='' /></a>
                             <a href='#'><img src={navIcon3} alt='' /></a>
                         </div>
