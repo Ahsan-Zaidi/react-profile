1ebd5f3abf0fd5ef38becd6d8e6fd33c6794dab3
Zaidi <zaidiahsan.r@gmail.com>
2023-01-24 16:19:32 -0600

    footer styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/img/logo.svg';
+import logo2 from '../assets/img/logo-footer.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -31,7 +31,7 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="/">
-                    <img src={logo} alt='Logo' />
+                    <img src={logo2} alt='Logo' />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className='navbar-toggler-icon'></span>
@@ -44,9 +44,9 @@ export const NavBar = () => {
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='https://www.linkedin.com/in/ahsan-zaidi-25641b254/' target="_blank"><img src={navIcon1} alt='' /></a>
-                            <a href='https://github.com/Ahsan-Zaidi' target="_blank"><img src={navIcon2} alt='' /></a>
-                            <a href='https://www.instagram.com/' target="_blank"><img src={navIcon3} alt='' /></a>
+                            <a href='https://www.linkedin.com/in/ahsan-zaidi-25641b254/' target="_blank" rel="noreferrer"><img src={navIcon1} alt='' /></a>
+                            <a href='https://github.com/Ahsan-Zaidi' target="_blank" rel="noreferrer"><img src={navIcon2} alt='' /></a>
+                            <a href='https://www.instagram.com/' target="_blank" rel="noreferrer"><img src={navIcon3} alt='' /></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('Connect')}>
                             <span>Let's Connect</span>
