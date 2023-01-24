a8f3a9698b00cf46c4ffcdad8776a1b979a91a2a
Zaidi <zaidiahsan.r@gmail.com>
2023-01-24 13:13:14 -0600

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,5 @@
+import { Nav, Tab } from "react-bootstrap";
+
 export const Projects = () => {
     const projects = [
         {
@@ -39,6 +41,34 @@ export const Projects = () => {
                     <Col>
                         <h2>Projects</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Ipsum has been the industry's standard dummy text.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" defaultActiveKey="/home">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <p>{project.title}</p>
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">Lorem</Tab.Pane>
+                                <Tab.Pane eventKey="third">Lorem</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
