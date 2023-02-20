a45be6fa607624c95a36f6561d39a3a33e5dcff8
Zaidi <zaidiahsan.r@gmail.com>
2023-02-20 16:53:12 -0600

    demos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,7 @@ import projImg8 from '../assets/img/project-img8.jpg';
 import projImg9 from '../assets/img/project-img9.jpg';
 import projImg10 from '../assets/img/project-img10.jpg';
 import projImg11 from '../assets/img/project-img11.jpg';
+import projImg12 from '../assets/img/project-img12.jpg';
 import colorSharp2 from '../assets/img/color-sharp2.png';
 
 export const Projects = () => {
@@ -41,7 +42,7 @@ export const Projects = () => {
             description: "Generate an image using AI",
             imgUrl: projImg4,
             Git: "https://github.com/Ahsan-Zaidi/ai-image-generator",
-            demo: "https://github.com/Ahsan-Zaidi/ai-image-generator",
+            demo: "https://ai-imager-generator.herokuapp.com/",
         },
         {
             title: "Password Generator",
@@ -65,28 +66,35 @@ export const Projects = () => {
             description: "Backend for social network application",
             imgUrl: projImg7,
             Git: "https://github.com/Ahsan-Zaidi/social-network-api",
-            demo: "https://github.com/Ahsan-Zaidi/social-network-api/blob/main/assets/DEMO.gif",
+            demo: "https://drive.google.com/file/d/1ZwwHqGmNV0h2rFRgYWEclB5eyTrkMZgY/view",
         },
         {
             title: "E-commerceBackend",
             description: "Backend for E-commerce website",
             imgUrl: projImg8,
             Git: "https://github.com/Ahsan-Zaidi/E-commerceBackEnd",
-            demo: "https://github.com/Ahsan-Zaidi/E-commerceBackEnd/blob/main/assets/images/Demo.gif",
+            demo: "https://drive.google.com/file/d/1i6h5b16PV9I6zQcw4MDxrY1itr1n1tIz/view",
         },
         {
             title: "Employee tracker",
             description: "Create employee db and tracker",
             imgUrl: projImg9,
             Git: "https://github.com/Ahsan-Zaidi/employee-tracker",
-            demo: "https://github.com/Ahsan-Zaidi/employee-tracker/blob/main/assets/demo/Demos.gif",
+            demo: "https://drive.google.com/file/d/1X21YXxitS_ct1jO-uGpZrInnYf5zCf98/view",
         },
         {
             title: "Team Profile Generator",
             description: "Generate a team",
             imgUrl: projImg10,
             Git: "https://github.com/Ahsan-Zaidi/Team-profile-generator",
-            demo: "https://github.com/Ahsan-Zaidi/Team-profile-generator/blob/main/src/demo.webm",
+            demo: "https://drive.google.com/file/d/1p0kVXyS-H-BAyzsLsza3SgoFIVw4tzEb/view",
+        },
+        {
+            title: "Readme-Generator",
+            description: "Generate a Readme for your app",
+            imgUrl: projImg12,
+            Git: "https://github.com/Ahsan-Zaidi/Readne-generator",
+            demo: "https://drive.google.com/file/d/1GknSzP1lqrjLOETl662ufiwEhvzGG1QA/view",
         },
     ];
 
@@ -96,7 +104,7 @@ export const Projects = () => {
             description: "Managment System",
             imgUrl: projImg11,
             Git: "https://github.com/Ahsan-Zaidi/project-mgmt",
-            demo: "https://github.com/Ahsan-Zaidi/project-mgmt",
+            demo: "https://drive.google.com/file/d/1Bh4CPU0IiauVqCoH7DlU9Dydv5tdVp94/view",
         },
     ];
 
